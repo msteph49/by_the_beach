4adae70636d247f89b01ae085b0bdbc557bde653
Michael Stephens <mdstephens26@yahoo.com>
2021-07-23 22:27:16 -0400

    Added Name and Phone_Number to New & Edit forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
     respond_to :html, :json
-    before_action :authenticate_user!
+    # before_action :authenticate_user!
+    protect_from_forgery with: :exception
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+         def configure_permitted_parameters
+              devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :phone_number, :email, :password)}
+
+              devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :phone_number, :email, :password, :current_password)}
+         end
     
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+    
+    
     def index
+        @user = User.find_by(params[:id])
     end
 end
