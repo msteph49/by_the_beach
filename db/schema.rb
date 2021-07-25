5015869bd2be96173f7d67d96022ce257b006d18
Michael Stephens <mdstephens26@yahoo.com>
2021-07-25 19:20:37 -0400

    Modified property forms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_25_182541) do
+ActiveRecord::Schema.define(version: 2021_07_25_224311) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2021_07_25_182541) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false
+    t.text "description"
     t.index ["user_id"], name: "index_properties_on_user_id"
   end
 
